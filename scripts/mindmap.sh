70f651d4dd2bcbad14cf4c6b878af68930cb15df
fahad <fahad@mpower-social.com>
2024-12-09 13:40:38 +0600

    mindmap update

--- scripts/mindmap.sh
+++ scripts/mindmap.sh
@@ -10,12 +10,19 @@ until mysql -h 192.168.100.10 -uopenmrs -popenmrs -e "SHOW TABLES;" openmrs > /d
   sleep 10
 done
 
-# Create the 'mindmap_server' database if it doesn't exist
-echo "Creating 'mindmap_server' database..."
-mysql -h 192.168.100.10 -uroot -popenmrs -e "CREATE DATABASE IF NOT EXISTS mindmap_server;"
+# Check if the 'mindmap_server' database exists
+DB_EXISTS=$(mysql -h 192.168.100.10 -uroot -popenmrs -e "SHOW DATABASES LIKE 'mindmap_server';" | grep "mindmap_server" > /dev/null 2>&1 && echo "yes" || echo "no")
 
-# Import SQL data into the 'mindmap_server' database
-echo "Importing data into 'mindmap_server'..."
-mysql -h 192.168.100.10 -uroot -popenmrs mindmap_server < /root/DB/mindmap_server.sql
+if [ "$DB_EXISTS" == "no" ]; then
+  # Create the database
+  echo "Creating 'mindmap_server' database..."
+  mysql -h 192.168.100.10 -uroot -popenmrs -e "CREATE DATABASE mindmap_server;"
+
+  # Import SQL data into the database
+  echo "Importing data into 'mindmap_server'..."
+  mysql -h 192.168.100.10 -uroot -popenmrs mindmap_server < /root/DB/mindmap_server.sql
+else
+  echo "Database 'mindmap_server' already exists. Skipping import."
+fi
 
 echo "Database setup complete."
